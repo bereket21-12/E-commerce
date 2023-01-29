132c1ee355d3c5e0d8449b90d5af257f047a23e5
Abel <abelzeleke5173@gmail.com>
2023-01-29 21:18:30 +0300

    made some changes on sign up

--- Template/signup.php
+++ Template/signup.php
@@ -1,35 +1,48 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/signup.css">
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <link rel="stylesheet" href="./css/signup.css">
+  <title>Sign Up</title>
 </head>
 <body>
-     <div class = "center">
-        <h1>sign up</h1>
-        <form method="post">
-     <div class="txt_field">
-        <input type=" text" required>
-        <span></span>
-        <label> Name</label>
-     </div>
-     <div class="txt_field">
-        <input type=" email" required>
-        <span></span>
-        <label> email</label>
-     </div>
-     <div class="txt_field">
-        <input type="password" required>
-        <span></span>
-        <label> Password</label>
-     </div>
-     
-     <input type="submit" value ="Sign up">
-    
+  <div class="container mt-5">
+    <h2 class="text-center">Sign Up</h2>
+    <form>
+      <div class="form-group">
+        <label for="firstName">First Name</label>
+        <input type="text" class="form-control" id="firstName" placeholder="Enter First Name">
+      </div>
+      <div class="form-group">
+        <label for="lastName">Last Name</label>
+        <input type="text" class="form-control" id="lastName" placeholder="Enter Last Name">
+      </div>
+      <div class="form-group">
+        <label for="email">Email</label>
+        <input type="email" class="form-control" id="email" placeholder="Enter Email">
+      </div>
+      <div class="form-group">
+        <label for="password" autocomplete="off">Password</label>
+        <input type="password" class="form-control" id="password" placeholder="Enter Password">
+      </div>
+      <div class="form-group">
+        <label for="confirmPassword">Confirm Password</label>
+        <input type="password" class="form-control" id="confirmPassword" placeholder="Confirm Password">
+      </div>
+      <div class="form-group">
+        <label for="lastName">Address 1</label>
+        <input type="text" class="form-control" id="lastName" placeholder="Enter Last Name">
+      </div><div class="form-group">
+        <label for="lastName">Address 2</label>
+        <input type="text" class="form-control" id="lastName" placeholder="Enter Last Name">
+      </div><div class="form-group">
+        <label for="lastName">Address 3</label>
+        <input type="text" class="form-control" id="lastName" placeholder="Enter Last Name">
+      </div>
+      <button type="submit" class="btn btn-primary btn-block">Submit</button>
     </form>
-     </div>
+  </div>
 </body>
 </html>
