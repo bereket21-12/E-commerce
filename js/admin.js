1f4d9c0a220dd76510d1db98378c40ce90b4e782
Abel <abelzeleke5173@gmail.com>
2023-01-31 19:27:53 +0300

    preparing the folders

--- js/admin.js
+++
@@ -1,8 +0,0 @@
-let proform = document.getElementById("addform");
-function clickHandler() {
-
-    proform.className = "container-fluid d-flex align-items-center flex-column border-dark border-secondary opacity-1";
-    console.log("clicked")
-}
-
-
