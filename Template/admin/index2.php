d51e7042901e8aee47c3b06f1be5932ad97e1e31
Bereket <berekettadelemulu210@gmail.com>
2023-01-31 20:09:23 +0300

    admin page added

---
+++ Template/admin/index2.php
@@ -0,0 +1,16 @@
+<?php
+
+include("./common/header.php");
+include("./common/navbar.php");
+include("./common/sidebar.php");
+
+?>
+
+<?php
+
+include("./pages/forms/addprocuct.php");
+?>
+
+<?php
+include("./common/footer.php");
+?>
---
+++ Template/admin/pages/forms/addprocuct.php
@@ -0,0 +1,47 @@
+<section class="content">
+      <div class="container-fluid">
+<div class="card card-primary">
+              <div class="card-header " style = "padding-left :300px">
+                <h3 class="card-title">Quick Example</h3>
+              </div>
+              <!-- /.card-header -->
+              <!-- form start -->
+              <form>
+                <div class="card-body" style = "padding-left :300px">
+                  <div class="form-group">
+                    <label for="exampleInputEmail1">Product Name</label>
+                    <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Enter Product Name">
+                  </div>
+                  <div class="form-group">
+                    <label for="exampleInputPassword1">Product Description</label>
+                    <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Enter Product Description">
+                  </div>
+                  <div class="form-group">
+                    <label for="exampleInputPassword1">Price</label>
+                    <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Enter Price">
+                  </div>
+                  <div class="form-group">
+                    <label for="exampleInputFile">File input</label>
+                    <div class="input-group">
+                      <div class="custom-file">
+                        <input type="file" class="custom-file-input" id="exampleInputFile">
+                        <label class="custom-file-label" for="exampleInputFile">Choose file</label>
+                      </div>
+                      <div class="input-group-append">
+                        <span class="input-group-text">Upload</span>
+                      </div>
+                    </div>
+                  </div>
+               
+                </div>
+                <!-- /.card-body -->
+
+                <div class="card-footer" style = "padding-left :300px">
+                  <button type="submit" class="btn btn-primary">Add</button>
+                </div>
+              </form>
+            </div>
+            <!-- /.card -->
+</div>
+</section>
+
