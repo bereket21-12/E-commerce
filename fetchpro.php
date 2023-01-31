d2b4f0cdc11b352cdabde69d7b666d5a7a24e66d
Bereket <berekettadelemulu210@gmail.com>
2023-01-31 23:15:32 +0300

    file clean

---
+++ fetchpro.php
@@ -0,0 +1,28 @@
+<?php
+
+include "./database/Dbcontroller.php";
+
+$db = new DBConnection();
+$conn = $db->getConnection();
+
+$sql = "SELECT * FROM product";
+$result = mysqli_query($conn, $sql); 
+
+if (mysqli_num_rows($result) > 0) {
+    while($row = mysqli_fetch_assoc($result)) {
+      $products[] = array(
+        "id" => $row["id"],
+        "product_name" => $row["product_name"],
+        "product_description" => $row["product_description"],
+        "product_img_path" => $row["image_url"]
+
+      );
+    }
+  } else {
+    echo "0 results";
+  }
+  
+  mysqli_close($conn);
+
+
+?>
---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+ 
+  include("./Commen/header.php")
+?>
+
+<?php
+ 
+  include("./Commen/navbar.php");
+
+  include("carousel.php");
+
+  include("catagory.php");
+
+  include("featured_products.php");
+
+  include("recent_products.php");
+
+  include("vendor.php");
+
+?>
+
+<?php
+include("./Commen/footer.php");
+
+?>
