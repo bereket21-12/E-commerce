7f25978578d26d6ec72f9a959881d43043e52390
Abel <abelzeleke5173@gmail.com>
2023-02-01 11:09:51 +0300

    made some changes in shop, login, contacts pages and removed Commen directory

--- contacts.php
+++ contacts.php
@@ -1,104 +1,76 @@
-   <?php
+<?php
 
-   include("./Commen/header.php");
-   include("./Commen/navbar.php");
-   ?>
-   
-    <!-- Contact Start -->
-    <div class="container-fluid" style=" margin-top: 3rem;">
-      <h2 class="section-title position-relative text-uppercase mx-xl-5 mb-4">
-        <span class="bg-secondary pr-3">Contact Us</span>
-      </h2>
-      <div class="row px-xl-5">
-        <div class="col-lg-7 mb-5">
-          <div class="contact-form bg-light p-30">
-            <div id="success"></div>
-            <form name="sentMessage" id="contactForm" novalidate="novalidate">
-              <div class="control-group">
-                <input
-                  type="text"
-                  class="form-control"
-                  id="name"
-                  placeholder="Your Name"
-                  required="required"
-                  data-validation-required-message="Please enter your name"
-                />
-                <p class="help-block text-danger"></p>
-              </div>
-              <div class="control-group">
-                <input
-                  type="email"
-                  class="form-control"
-                  id="email"
-                  placeholder="Your Email"
-                  required="required"
-                  data-validation-required-message="Please enter your email"
-                />
-                <p class="help-block text-danger"></p>
-              </div>
-              <div class="control-group">
-                <input
-                  type="text"
-                  class="form-control"
-                  id="subject"
-                  placeholder="Subject"
-                  required="required"
-                  data-validation-required-message="Please enter a subject"
-                />
-                <p class="help-block text-danger"></p>
-              </div>
-              <div class="control-group">
-                <textarea
-                  class="form-control"
-                  rows="8"
-                  id="message"
-                  placeholder="Message"
-                  required="required"
-                  data-validation-required-message="Please enter your message"
-                ></textarea>
-                <p class="help-block text-danger"></p>
-              </div>
-              <div>
-                <button
-                  class="btn btn-primary py-2 px-4"
-                  type="submit"
-                  id="sendMessageButton"
-                >
-                  Send Message
-                </button>
-              </div>
-            </form>
+include("./Common/header.php");
+include("./Common/navbar.php");
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $subject = $_POST['subject'];
+  $message = $_POST['message'];
+
+  // Use PHP's mail() function to send the email
+  $to = "abelzeleke5173@gmail.com";
+  $headers = "From: $email\r\n";
+  $headers .= "Reply-To: $email\r\n";
+  mail($to, $subject, $message, $headers);
+
+  echo "Your message has been sent!";
+}
+
+?>
+
+<!-- Contact Start -->
+<div class="container-fluid" style=" margin-top: 3rem;">
+  <h2 class="section-title position-relative text-uppercase mx-xl-5 mb-4">
+    <span class="bg-secondary pr-3">Contact Us</span>
+  </h2>
+  <div class="row px-xl-5">
+    <div class="col-lg-7 mb-5">
+      <div class="contact-form bg-light p-30">
+        <div id="success"></div>
+        <form name="sentMessage" id="contactForm" method="post" novalidate="novalidate">
+          <div class="control-group">
+            <input type="text" class="form-control" id="name" name="name" placeholder="Your Name" required="required" data-validation-required-message="Please enter your name" />
+            <p class="help-block text-danger"></p>
           </div>
-        </div>
-        <!-- <div class="col-lg-5 mb-5">
-          <div class="bg-light p-30 mb-30">
-            <iframe
-              style="width: 100%; height: 250px"
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3001156.4288297426!2d-78.01371936852176!3d42.72876761954724!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4ccc4bf0f123a5a9%3A0xddcfc6c1de189567!2sNew%20York%2C%20USA!5e0!3m2!1sen!2sbd!4v1603794290143!5m2!1sen!2sbd"
-              frameborder="0"
-              style="border: 0"
-              allowfullscreen=""
-              aria-hidden="false"
-              tabindex="0"
-            ></iframe>
-          </div> -->
-          <div class="bg-light p-30 mb-3">
-            <p class="mb-2">
-              <i class="fa fa-map-marker-alt text-primary mr-3"></i>123 Street,
-              Wolkite, WKU
-            </p>
-            <p class="mb-2">
-              <i class="fa fa-envelope text-primary mr-3"></i>info@example.com
-            </p>
-            <p class="mb-2">
-              <i class="fa fa-phone-alt text-primary mr-3"></i>+251 945 67890
-            </p>
+          <div class="control-group">
+            <input type="email" class="form-control" id="email" name="email" placeholder="Your Email" required="required" data-validation-required-message="Please enter your email" />
+            <p class="help-block text-danger"></p>
           </div>
-        </div>
+          <div class="control-group">
+            <input type="text" class="form-control" id="subject" name="subject" placeholder="Subject" required="required" data-validation-required-message="Please enter a subject" />
+            <p class="help-block text-danger"></p>
+          </div>
+          <div class="control-group">
+            <textarea class="form-control" rows="8" id="message" name="message" placeholder="Message" required="required" data-validation-required-message="Please enter your message"></textarea>
+            <p class="help-block text-danger"></p>
+          </div>
+          <div>
+            <button class="btn btn-primary py-2 px-4" type="submit" id="sendMessageButton">
+              Send Message
+            </button>
+          </div>
+        </form>
       </div>
     </div>
-    <!-- Contact End -->
-    <?php
+    <div class="bg-light p-30 mb-3" style="overflow: auto;">
+      <p class="mb-2">
+        <i class="fa fa-map-marker-alt text-primary mr-3"></i>123 Street,
+        Wolkite, WKU
+      </p>
+      <p class="mb-2">
+        <i class="fa fa-envelope text-primary mr-3"></i>abelzeleke5173@gmail.com
+      </p>
+      <p class="mb-2">
+        <i class="fa fa-phone-alt text-primary mr-3"></i>+251 935 353626
+      </p>
+    </div>
+  </div>
+</div>
+</div>
+<!-- Contact End -->
+<?php
 
-    include("./Commen/footer.php");
-    ?>
+include("./Commen/footer.php");
+?>
--- login.php
+++ login.php
@@ -1,33 +1,65 @@
+<!-- php
+session_start();
+
+// Connect to the database
+$connection = mysqli_connect("localhost", "user", "password", "database");
+
+// Check if the user has submitted the form
+if (isset($_POST['email']) && isset($_POST['password'])) {
+   // Get the values from the form
+   $email = mysqli_real_escape_string($connection, $_POST['email']);
+   $password = mysqli_real_escape_string($connection, $_POST['password']);
+
+   // Retrieve the user data from the database
+   $query = "SELECT * FROM users WHERE email='$email' AND password='$password'";
+   $result = mysqli_query($connection, $query);
+
+   // Check if the user exists
+   if (mysqli_num_rows($result) == 1) {
+      // User exists, set the session variables
+      $user = mysqli_fetch_assoc($result);
+      $_SESSION['email'] = $user['email'];
+      $_SESSION['logged_in'] = true;
+
+      // Redirect the user to the protected page
+      header("Location: index.php");
+      exit;
+   }
+}
+ -->
+
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/login.css">
-    <title>Document</title>
-</head>
-<body>
-     <div class = "center">
-        <h1>Login </h1>
-        <form method="post" action = "logincontroller.php">
-     <div class="txt_field">
-        <input type=" text" name = "email" required>
-        <span></span>
-        <label> Username</label>
-     </div>
-     <div class="txt_field">
-        <input type="password" name ="password" required>
-        <span></span>
-        <label> Password</label>
-     </div>
-     <div class="pass">Forgot Password?</div>
-     <input type="submit" value ="Login">
-     <div class="signup_link">
-       Not a member? <a href="./signup.php">Signup</a>
-     </div>
-    </form>
-     </div>
-</body>
-</html>
 
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="stylesheet" href="./css/login.css">
+   <title>Document</title>
+</head>
+
+<body>
+   <div class="center">
+      <h1>Login </h1>
+      <form method="post" action="">
+         <div class="txt_field">
+            <input type="text" name="email" required>
+            <span></span>
+            <label>Username</label>
+         </div>
+         <div class="txt_field">
+            <input type="password" name="password" required>
+            <span></span>
+            <label>Password</label>
+         </div>
+         <div class="pass">Forgot Password?</div>
+         <input type="submit" value="Login">
+         <div class="signup_link">
+            Not a member? <a href="./signup.php">Signup</a>
+         </div>
+      </form>
+   </div>
+</body>
+
+</html>
