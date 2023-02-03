85219f0fee093273220e612b2a1b967e78499161
Abel <abelzeleke5173@gmail.com>
2023-02-03 15:31:35 +0300

    made some chages in admin and navbar files

--- login_handler.php
+++ login_handler.php
@@ -18,16 +18,13 @@ if (isset($_POST['username']) && isset($_POST['password'])) {
    $password = $_POST['password'];
 
    $query = "SELECT * FROM Customer WHERE username='$username' AND password='$password'";
-   // $query = "SELECT role FROM Customer WHERE username = '$username' AND password = '$password'";
    $result = mysqli_query($conn, $query);
-   // $result2 = mysqli_query($conn, $query);
 
    if (mysqli_num_rows($result) == 1) {
 
       $user = mysqli_fetch_assoc($result);
-      // $row = mysqli_fetch_assoc($result2);
       $role = $user['role'];
-      // Store the role in a session variable
+
       $_SESSION['logged_in'] = true;
       $_SESSION['username'] = $user['username'];
       $_SESSION['role'] = $role;
@@ -38,17 +35,7 @@ if (isset($_POST['username']) && isset($_POST['password'])) {
          header("Location: index.php");
       }
    } else {
-      // If no match was found, show an error message
       echo "Incorrect username or password";
    }
 }
-
 ?>
-
-<!-- <html>
-<body>
-
-<h1>Hello World!!</h1>
-
-</body>
-</html> -->
