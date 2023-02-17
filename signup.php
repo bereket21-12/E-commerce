9f83cacff323283e8537049a028d59667e70a7e7
Bereket <berekettadelemulu210@gmail.com>
2023-02-17 12:31:47 +0300

    final ip

--- signup.php
+++ signup.php
@@ -1,55 +1,58 @@
-
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-  <link rel="stylesheet" href="./css/signup.css">
-  <title>Sign Up</title>
-  <style>
-input:focus:invalid{
-  border-color : red;
-}
-input:focus:valid{
-  border-color : green;
-}
-</style>
 
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/signup.css">
+    <title>Document</title>
 </head>
+
 <body>
-  <div class="container mt-5">
-    <h2 class="text-center">Sign Up</h2>
-    <form  method ="post" action="./signup_controller.php">
-      <div class="form-group">
-        <label for="fname" required>First Name</label>
-        <input name ="fname" type="text" class="form-control" id="firstName" placeholder="Enter First Name">
-      </div>
-      <div class="form-group">
-        <label for="lname">Last Name</label>
-        <input name = "lname" type="text" class="form-control" id="lastName" placeholder="Enter Last Name">
-      </div>
-      <div class="form-group">
-        <label for="email">Email</label>
-        <input name= "email" type="email" class="form-control" id="email" placeholder="Enter Email" required>
-      </div>
-      <div class="form-group">
-        <label for="password" autocomplete="off" required>Password</label>
-        <input name = "password" type="password" class="form-control" id="password" placeholder="Enter Password">
-      </div>
-      <div class="form-group">
-        <label for="confirmPassword">Confirm Password</label>
-        <input type="password" class="form-control" id="confirmPassword" placeholder="Confirm Password">
-      </div>
-      <div class="form-group">
-        <label for="address">Address</label>
-        <input name = "address" type="text" class="form-control" id="lastName" placeholder="Enter Last Name">
-      </div><div class="form-group">
-        <label >Phone No.</label>
-        <input name = "phone" type="tel" class="form-control" id="phone" placeholder="Enter Phone number" required>
-      </div>
-      <button type="submit" class="btn btn-primary btn-block">Submit</button>
-    </form>
+  <div class="container">
+    <div class="title">Signup</div>
+    <div class="content">
+      <form action="signup_controller.php" method="post">
+        <div class="user-details">
+          <div class="input-box">
+            <span class="details">First Name</span>
+            <input type="text" name="firstName" placeholder="Enter your first name" required />
+          </div>
+          <div class="input-box">
+            <span class="details">Last Name</span>
+            <input type="text" name="lastName" placeholder="Enter your last name" required />
+          </div>
+          <div class="input-box">
+            <span class="details">Username</span>
+            <input type="text" name="username" placeholder="Enter your username" required />
+          </div>
+          <div class="input-box">
+            <span class="details">Email</span>
+            <input type="email" name="email" placeholder="Enter your email" required />
+          </div>
+          <div class="input-box">
+            <span class="details">Address</span>
+            <input type="text" name="address" placeholder="Enter your Address" required />
+          </div>
+          <div class="input-box">
+            <span class="details">Password</span>
+            <input type="password" name="password" placeholder="Enter your password" required />
+          </div>
+          <div class="input-box">
+            <span class="details">Confirm Password</span>
+            <input type="password" name="confirmPassword" placeholder="Confirm your password" required />
+          </div>
+        </div>
+        <div class="button">
+          <input type="submit" name="submit" value="Signup" />
+          <div class="login_link">
+            <a href="./login.php">Back to Login</a>
+          </div>
+        </div>
+      </form>
+    </div>
   </div>
 </body>
+
 </html>
--- signup_controller.php
+++ signup_controller.php
@@ -7,21 +7,21 @@ include "./database/Dbcontroller.php";
 
 $db = new DBConnection();
 $conn = $db->getConnection();
+if (isset($_POST['submit'])) {
 
-
-    $fname = $_POST['fname'];
-    $lname = $_POST['lname'];
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-    $address = $_POST['address'];
-    $phone_number =$_POST['phone'];
+   $firstName = $_POST['firstName'];
+  $lastName = $_POST['lastName'];
+  $username = $_POST['username'];
+  $email = $_POST['email'];
+  $address = $_POST['address'];
+  $password = $_POST['password'];
+  $confirmPassword = $_POST['confirmPassword'];
     
-    $sql = "INSERT INTO user (fname,lname,email,password,address,phone_number) 
-    VALUES (?,?,?,?,?,?)";
+   $sql = "INSERT INTO Customer (first_name,last_name,email,password,address,confirmed,username) VALUES (?,?,?,?,?,?,?)";
     
     
     $stmt = $conn->prepare($sql);
-    $stmt->bind_param("ssssss", $fname, $lname, $email ,$password,$address,$phone_number);
+    $stmt->bind_param("sssssss",$firstName, $lastName, $email ,$password,$address, $confirmPassword, $username);
 
     if($stmt->execute()){
         echo "Information submitted successfully";
@@ -30,6 +30,8 @@ $conn = $db->getConnection();
         header("Location: login.php");
     } else{
         echo "Error: " . $stmt->error;
+    }
+    
     }
     
     mysqli_close($conn);
